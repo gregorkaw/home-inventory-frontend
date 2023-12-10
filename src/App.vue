fa194670ffedc7da48196a787a66251fffd38268
Gregor <grzegorzkaw@gmail.com>
2023-12-10 18:10:46 +0100

    Initialization of the app

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld msg="Home inventory" />
+</template>
+
+<style scoped>
+
+</style>
